387470729db22c293941618952151e75cfdf51a8
Steven Yoo <yooste1989@gmail.com>
2023-02-08 11:23:45 -0600

    adding dialyzer to mix

--- mix.exs
+++ mix.exs
@@ -40,7 +40,8 @@ defmodule FoodTruck.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
     ]
   end
 
